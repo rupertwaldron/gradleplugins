c89a12331f7379be642132af0db713b1e9efb0ac
rupertwaldron <rupert.waldron@yahoo.co.uk>
2022-11-12 15:41:17 +0000

    can get result

--- greeting-plugin/src/main/java/com/example/plugin/GreetingPlugin.java
+++ greeting-plugin/src/main/java/com/example/plugin/GreetingPlugin.java
@@ -1,18 +1,23 @@
 package com.example.plugin;
 
+import com.example.plugin.tasks.SumTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class GreetingPlugin implements Plugin<Project> {
-    public void apply(Project project) {
+  public void apply(Project project) {
 
-        GreetingPluginExtension extension = project.getExtensions()
-                                                       .create("greet", GreetingPluginExtension.class);
+    project.getExtensions().create("calculation", GreetingPluginExtension.class);
 
-        project.task("greet").doLast(task -> {
-            int sum = extension.getNum1() + extension.getNum2();
+//        project.task("greet").doLast(task -> {
+//            int sum = extension.getNum1() + extension.getNum2();
+//            extension.setResult(sum);
+//
+//            System.out.println("Hello, " + extension.getNum1() + " + " + extension.getNum2() + " = " + sum);
+//        });
 
-            System.out.println("Hello, " + extension.getNum1() + " + " + extension.getNum2() + " = " + sum);
-        });
-    }
+    project.getTasks().create("sum", SumTask.class).setGroup("calculation");
+
+
+  }
 }
---
+++ greeting-plugin/src/main/java/com/example/plugin/tasks/SumTask.java
@@ -0,0 +1,23 @@
+package com.example.plugin.tasks;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskAction;
+
+public abstract class SumTask extends DefaultTask {
+
+  @Input
+  public abstract Property<Integer> getNum1();
+
+  @Input
+  public abstract Property<Integer> getNum2();
+
+  @TaskAction
+  public int calculate() {
+    int output = getNum1().get() + getNum2().get();
+    System.out.println("Calculating the result... " + output);
+    return output;
+  }
+
+}
