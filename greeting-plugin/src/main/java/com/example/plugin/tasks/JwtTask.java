97e0d6110c5a49530672643faa6e5ab8769a51e8
rupertwaldron <rupert.waldron@yahoo.co.uk>
2022-11-13 14:17:31 +0000

    working with basic http request

--- greeting-plugin/src/main/java/com/example/plugin/tasks/JwtTask.java
+++ greeting-plugin/src/main/java/com/example/plugin/tasks/JwtTask.java
@@ -5,15 +5,29 @@ import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public abstract class JwtTask extends DefaultTask {
 
   @Input
   public abstract Property<String> getJwtPath();
 
   @TaskAction
-  public String goAndFetchJwt() {
-    System.out.println("Getting jwt from " + getJwtPath().get());
-    return "Returned JWT";
+  public String goAndFetchJwt() throws IOException {
+    StringBuilder result = new StringBuilder();
+    URL url = new URL(getJwtPath().get());
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    conn.setRequestMethod("GET");
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+      for (String line; (line = reader.readLine()) != null; ) {
+        result.append(line);
+      }
+    }
+    return "Returned JWT -> " + result;
   }
 
 }
