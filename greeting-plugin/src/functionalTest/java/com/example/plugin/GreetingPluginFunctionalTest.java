bbf65aa1b234c33de0a7aeb11ac1d5af75919b66
rupertwaldron <rupert.waldron@yahoo.co.uk>
2022-11-14 19:30:01 +0000

    wiremock ok

--- greeting-plugin/src/functionalTest/java/com/example/plugin/GreetingPluginFunctionalTest.java
+++ greeting-plugin/src/functionalTest/java/com/example/plugin/GreetingPluginFunctionalTest.java
@@ -1,5 +1,7 @@
 package com.example.plugin;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
 import org.junit.jupiter.api.Test;
@@ -10,61 +12,74 @@ import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+@WireMockTest(httpPort = 9000)
 public class GreetingPluginFunctionalTest {
-    @Test
-    public void canRunTask() throws IOException {
-        // Setup the test build
-        File projectDir = new File("build/functionalTest");
-        Files.createDirectories(projectDir.toPath());
-        writeString(new File(projectDir, "settings.gradle"), "");
 
-        String buildGradle = """
-            plugins {
-                id('com.example.greeting')
-            }
-            
-            def output = sum {
-                num1 = 1
-                num2 = 2
-            }.calculate()
-            
-            def output2 = sub {
-                num1 = 1
-                num2 = 2
-            }.calculate()
-            
-            println "1 + 2 = ${output}"
-            println "1 - 2 = ${output2}"
-            
-            def jwtToken = jwt {
-                 jwtPath = "https://ed2245ff-0869-4b36-9f82-5760a6f4ba6e.mock.pstmn.io/jwt"
-            }.goAndFetchJwt()
-            
-            println "jwt token = ${jwtToken}"
-            """;
+  private static final String APPLICATION_JSON = "application/json";
 
-        writeString(new File(projectDir, "build.gradle"), buildGradle);
+  @Test
+  public void canRunTask() throws IOException {
 
-        // Run the build
-        BuildResult result = GradleRunner.create()
-            .forwardOutput()
-            .withPluginClasspath()
-//            .withArguments("sum")
-            .withProjectDir(projectDir)
-            .build();
+    stubFor(get(WireMock.urlEqualTo("/api/jwt"))
+                  .willReturn(aResponse()
+                                  .withStatus(200)
+                                  .withHeader("Content-Type", APPLICATION_JSON)
+                                  .withBody("hello")));
 
-        // Verify the result
-        assertTrue(result.getOutput().contains("1 + 2 = 3"));
-        assertTrue(result.getOutput().contains("1 - 2 = -1"));
-        assertTrue(result.getOutput().contains("jwt token = Returned JWT -> hello"));
-    }
+    // Setup the test build
+    File projectDir = new File("build/functionalTest");
+    Files.createDirectories(projectDir.toPath());
+    writeString(new File(projectDir, "settings.gradle"), "");
 
-    private void writeString(File file, String string) throws IOException {
-        try (Writer writer = new FileWriter(file)) {
-            writer.write(string);
+    String buildGradle = """
+        plugins {
+            id('com.example.greeting')
         }
+                    
+        def output = sum {
+            num1 = 1
+            num2 = 2
+        }.calculate()
+                    
+        def output2 = sub {
+            num1 = 1
+            num2 = 2
+        }.calculate()
+                    
+        println "1 + 2 = ${output}"
+        println "1 - 2 = ${output2}"
+                    
+        def jwtToken = jwt {
+             jwtPath = "http://localhost:9000/api/jwt"
+        }.goAndFetchJwt()
+                    
+        println "jwt token = ${jwtToken}"
+        """;
+
+    writeString(new File(projectDir, "build.gradle"), buildGradle);
+
+    // Run the build
+    BuildResult result = GradleRunner.create()
+                             .forwardOutput()
+                             .withPluginClasspath()
+//            .withArguments("sum")
+                             .withProjectDir(projectDir)
+                             .build();
+
+    // Verify the result
+    assertTrue(result.getOutput().contains("1 + 2 = 3"));
+    assertTrue(result.getOutput().contains("1 - 2 = -1"));
+    assertTrue(result.getOutput().contains("jwt token = Returned JWT -> hello"));
+  }
+
+  private void writeString(File file, String string) throws IOException {
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(string);
     }
+  }
 }
